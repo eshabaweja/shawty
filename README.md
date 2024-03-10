42a6868992ba691b63879c827b7d30957f67d65c
eshabaweja <bawejaesha02@gmail.com>
2024-03-10 21:30:27 +0530

    feat: url encoded and stored in db

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 1. set up my repo and create a venv
 2. system design: [https://www.youtube.com/watch?v=JQDHz72OA3c](https://www.youtube.com/watch?v=JQDHz72OA3c)
 3. base62 encoding scheme: [string to decimal to base62](https://medium.com/@anabhishek.jha/base-62-text-encoding-decoding-b43921c7a954)
+4. or we could use [squid](https://github.com/sqids/sqids-python)
+5. To use [SQLite](https://www.digitalocean.com/community/tutorials/how-to-use-the-sqlite3-module-in-python-3)
+6. first run `utils/init_db.py` then `main.py`
--- main.py
+++ main.py
@@ -1,15 +1,22 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, request
 from utils import url_encode
+import sqlite3
 
 app = Flask(__name__)
 
 @app.route("/", methods = ["GET","POST"])
 def index():
+    connection = sqlite3.connect('database.db')
+    connection.row_factory = sqlite3.Row
+
     if request.method == "POST":
         long_url = request.form['long_url']
         short_url = url_encode.base62_encode(long_url)
-
         # map short url (key) to long url (value)
+        connection.execute('INSERT INTO urls VALUES (?, ?)',(short_url,long_url,))
+        connection.commit()
+        connection.close()
+
         return f"shawty URL: {request.url_root}{short_url}"
     return render_template("index.html")
 
@@ -18,7 +25,7 @@ def index():
 @app.route("/<short_url>")
 def redirect_url(short_url):
     # long_url = short_urls[short_url]
-    # use map of short url (key) to long url (value)
+    # use map of short url (key) to get long url (value)
     return "redirected to original URL"
 
 if __name__=="__main__":
