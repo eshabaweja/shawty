3c8ae449c68108af10c1291cea469f5b6b235e48
eshabaweja <bawejaesha02@gmail.com>
2024-03-10 17:27:40 +0530

    feat: routing

---
+++ main.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, url_for, request
+from utils import url_encode
+
+app = Flask(__name__)
+
+@app.route("/", methods = ["GET","POST"])
+def home():
+    if request.method == "POST":
+        long_url = request.form['long_url']
+        short_url = url_encode.base62_encode(long_url)
+
+        # map short url (key) to long url (value)
+        return f"shawty URL: {request.url_root}{short_url}"
+    return render_template("index.html")
+
+
+# redirecting the short url to original
+@app.route("/<short_url>")
+def redirect_url(short_url):
+    # long_url = short_urls[short_url]
+    # use map of short url (key) to long url (value)
+    return "redirected to original URL"
+
+if __name__=="__main__":
+    app.run(debug=True)
